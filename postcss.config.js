c88bc51cb759b7b185feddaf952b1e79c6203cc6
Jenil Gogari <jenil27@hotmail.com>
2018-07-29 17:45:05 -0400

    updates 🎨🔧💄⛔️🆗 * new variables * error-success states * striped tables * button upgrades * enhancements to colors and styles * text styles

--- postcss.config.js
+++ postcss.config.js
@@ -3,8 +3,14 @@ let plugins = [
   require("autoprefixer")({ browsers: "last 2 versions" })
 ];
 
-if (process.env.NODE_ENV == 'production') plugins.push(require("postcss-csso"))
+// if (process.env.NODE_ENV == 'production') plugins.push(require("postcss-csso"))
+if (process.env.NODE_ENV == "production")
+  plugins.push(
+    require("cssnano")({
+      preset: "default"
+    })
+  );
 
 module.exports = {
   plugins
-}
+};
