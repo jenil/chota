d8be45aa67d94175228732555266176a5d552e02
Jenil G <jenil27@hotmail.com>
2022-08-30 23:45:18 -0400

    add linter and format files

--- postcss.config.js
+++ postcss.config.js
@@ -1,16 +1,19 @@
 let plugins = [
-  require("postcss-import"),
-  require("autoprefixer")()
+  require("postcss-import")({
+    plugins: [require("stylelint")({ fix: true, formatter: "compact" })],
+  }),
+  require("autoprefixer")(),
+  require("postcss-reporter")({ clearReportedMessages: true }),
 ];
 
 // if (process.env.NODE_ENV == 'production') plugins.push(require("postcss-csso"))
 if (process.env.NODE_ENV == "production")
   plugins.push(
     require("cssnano")({
-      preset: "default"
+      preset: "default",
     })
   );
 
 module.exports = {
-  plugins
+  plugins,
 };
